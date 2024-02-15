c8887dce08325bf666d7846426a9af96f5f16dcd
lcy <15269230591@163.com>
2024-02-16 00:00:19 +0800

    增加了线程池，可以完成爬取

--- mooc.py
+++ mooc.py
@@ -6,6 +6,8 @@ from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from ip代理池 import cre_headers,cre_proxies
+from concurrent.futures import ThreadPoolExecutor
+import os
 
 def get_school_url():
     driver = webdriver.Edge()
@@ -20,8 +22,6 @@ def get_course_url(school_with_url):
     course = {}
     driver = webdriver.Edge()
     driver.get(school_with_url[1])
-    # print(driver.page_source)
-    # print(course_url)
     while(True):
         et = etree.HTML(driver.page_source)
         course_name = et.xpath('//span[@class="f-thide u-courseCardWithTime-teacher_span162"]/text()')
@@ -43,30 +43,44 @@ def get_course_inf(course):
     obj_inf = re.compile('enrollCount : "(?P<chooseCount>.*?)",.*?id : "(?P<id>.*?)"',re.S)
     obj_dir = re.compile('name: "(?P<dir_name>.*?)".*?goals: "(?P<dir_goals>.*?)".*?plan: "(?P<dir_plan>.*?)"',re.S)
     obj_teacher = re.compile('lectorName : "(?P<teacher_name>.*?)".*?lectorTitle : "(?P<teacher_title>.*?)"',re.S)
+    obj_dir_null = re.compile('overflow-wrap: break-word; font-size: 13px;\\\\">(?P<dir>.*?)<')
     result_inf = obj_inf.search(res.text)
     course.update(result_inf.groupdict())
     course['dir'] = obj_dir.findall(res.text)
+    if course['dir'] == []:
+        course['dir'] = obj_dir_null.findall(res.text)
     course['teacher'] = obj_teacher.findall(res.text)
     #名字、id、教师、目录、选课人数
     driver = webdriver.Edge()
     driver.get(url)
     driver.find_element(by=By.XPATH, value='//div[@id="review-tag-button"]').click()
     time.sleep(2)
-    # print(driver.page_source)
     et = etree.HTML(driver.page_source)
     result_scores = et.xpath('//div[@class="ux-mooc-comment-course-comment_head_rating-scores"]/span/text()')
     result_commentsCount = et.xpath('//div[@class="ux-mooc-comment-course-comment_head_rating-action_tips"]/span/text()')
-    course['scores'] = result_scores[0]
-    course['commentsCount'] = re.search('共 (?P<commentsCount>.*?) 条评价',result_commentsCount[0]).groupdict()['commentsCount']
-    #分数、评论数
-    print(course)
+    if not result_scores == []:
+        course['scores'] = result_scores[0]
+    else:
+        course['scores'] = '0'
+    if not result_commentsCount == []:
+        course['commentsCount'] = re.search('共 (?P<commentsCount>.*?) 条评价',result_commentsCount[0]).groupdict()['commentsCount']
+    else:
+        course['commentsCount'] = '0'
+        #分数、评论数
+    f_school = course['school']
+    f_name = course['name']
+    if not os.path.exists(f'F:/WorkSpace/mooc/{f_school}'):
+        os.mkdir(f'F:/WorkSpace/mooc/{f_school}')
+    f = open(f'F:/WorkSpace/mooc/{f_school}/{f_name}.txt', 'w', encoding='utf-8')
+    json_course = json.dumps(course, ensure_ascii=False)
+    f.write(json_course)
+    f.close()
 
 def main():
-    # school_with_url = get_school_url()
-    # a = [('北京大学', 'https://www.icourse163.org/university/PKU'),('error!!', 'error!!')]
-    # get_course_url(a[0])
-    a1 = {'name': '医学与文学', 'url': 'https://www.icourse163.org/course/PKU-1461784179', 'school': '北京大学'}
-    a2 = {'name': '经典昆曲欣赏', 'url': 'https://www.icourse163.org/course/PKU-1470426173', 'school': '北京大学'}
-    get_course_inf(a1)
+    school_with_url = get_school_url()
+    with ThreadPoolExecutor(10) as task:
+        for item in school_with_url:
+            task.submit(get_course_url, item)
+
 if __name__ =='__main__':
     main()
