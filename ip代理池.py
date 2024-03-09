382480d6ac560057c45cc7c2855d0c43d4c0e95b
lcy12384 <15269230591@163.com>
2024-03-09 23:23:53 +0800

    增加了额外的数据类型

--- ip代理池.py
+++ ip代理池.py
@@ -12,14 +12,14 @@ def cre_headers():
     return headers
 
 def get_ip_pool():
-    global proxies_pool
-    with open("F:/WorkSpace/ip_kuaidaili.json", "r") as load_f:
+    with open("D:/WorkSpace/毕业设计/ip_kuaidaili.json", "r") as load_f:
         load_dic = json.load(load_f)
         data = load_dic['data']
         proxies_pool = data['proxy_list']
+    return  proxies_pool
 
 def cre_proxies():
-    proxy = random.choice(proxies_pool)
+    proxy = random.choice(get_ip_pool())
     proxies = {
         "http": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": 'd2213256669', "pwd": 'xu04zwnc', "proxy": proxy},
         "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": 'd2213256669', "pwd": 'xu04zwnc', "proxy": proxy}
@@ -31,7 +31,7 @@ def ip_kuaidaili():
     url = "https://dps.kdlapi.com/api/getdps/?secret_id=ova46vu74f7tvnhwrd0w&num=20&signature=05aqi07it63y93hyj3oq9wi5wdhaf145&pt=1&dedup=1&format=json&sep=1"
     res = requests.get(url)
     print(type(res.json()))
-    with open("F:/WorkSpace/ip_kuaidaili.json", "w") as f:
+    with open("D:/WorkSpace/毕业设计/ip_kuaidaili.json", "w") as f:
         json.dump(res.json(), f)
 
 def ip_zhandaye():
@@ -52,7 +52,5 @@ def ip_zhandaye():
 def main():
     # ip_zhandaye()
     ip_kuaidaili()
-    get_ip_pool()
-
 if __name__ == "__main__":
     main()
--- 学银.py
+++ 学银.py
@@ -46,6 +46,35 @@ def get_course_inf(url, f):
     dic.update(json.loads(resp.text))
     dic['url'] = url
     #累计页面浏览量、累计选课人数、累计互动次数
+    resp = requests.get(url, headers=cre_headers(), proxies=cre_proxies())
+    et = etree.HTML(resp.text)
+    course_ex_inf = et.xpath("//dl[@class = 'mgCard_deta']/dt/span/@class")
+    if course_ex_inf == []:
+        dic['courseLevel'] = "普通课程"
+    else:
+        if course_ex_inf[0] == 'qTips_one':
+            dic['courseLevel'] = '国家一流课程'
+        else:
+            dic['courseLevel'] = '省精品课程'
+    # 课程等级
+    course_ex_inf = et.xpath("//span[@class = 'mgCard_dijiqi_con']/a/text()")
+    course_ex_inf_cur = et.xpath("//span[@class = 'mgCard_dijiqi_con']/a/@href")
+    course_ex_inf_cur = ["https://www.xueyinonline.com/" + i for i in course_ex_inf_cur]
+    course_ex_inf = [list(i) for i in zip(course_ex_inf, course_ex_inf_cur)]
+    dic['qici'] = course_ex_inf
+    # 期次
+    course_ex_inf = et.xpath("//dl[@class ='mgCard_deta']/dd[3]/text()")
+    dic['endtime'] = course_ex_inf
+    # 起止日期
+    course_ex_inf = et.xpath("//dl[@class ='mgCard_deta']/dd[4]/span/a[@class = 'current']/text()")
+    dic['status'] = course_ex_inf
+    # 教学进度
+    domain = 'https://www.xueyinonline.com/detail/introduce?courseId=' + dic['id'] + '&orgCourseId=' + dic['id']
+    resp_tem = requests.get(domain, headers=cre_headers(), proxies=cre_proxies())
+    et = etree.HTML(resp_tem.text)
+    course_ex_inf = et.xpath("//div[@class = 'kc_intro_text  clearAfter']/p[@style = 'white-space: normal;']/text()")
+    dic['introduce'] = course_ex_inf
+    # 说明、适用人群
     json_inf = json.dumps(dic, ensure_ascii=False)
     f.write(json_inf + '\n')
     return dic
@@ -76,13 +105,13 @@ def get_course_dir(url, f):
 
 def main():
     with ThreadPoolExecutor(10) as task:
-        for i in range(1, 1184):
+        for i in range(678, 1226):
             url = "https://xueyinonline.com/mooc/categorycourselist?categoryid=0&coursetype=0&page="
             url = url + f"{i}"
             url_result = get_course_url(url)
             # 课程名称、课程id、主讲教师、学校、累计页面浏览量、累计选课人数、累计互动次数、课程评分、评论数
-            f_inf = open(f'F:/WorkSpace/xueyin_course_inf/page{i}.txt', 'w', encoding='utf-8')
-            f_dir = open(f'F:/WorkSpace/xueyin_course_dir/page{i}.txt', 'w', encoding='utf-8')
+            f_inf = open(f'D:/WorkSpace/毕业设计/xueyin_course_inf-2024.3.7/page{i}.txt', 'w', encoding='utf-8')
+            f_dir = open(f'D:/WorkSpace//毕业设计/xueyin_course_dir-2024.3.7/page{i}.txt', 'w', encoding='utf-8')
             for course_url in url_result:
                 task.submit(lambda cxp:get_course_inf(*cxp), (course_url, f_inf))
                 task.submit(lambda cxp:get_course_dir(*cxp), (course_url, f_dir))
