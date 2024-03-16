c8bbb711b2c70d3210c7aea8d651dec959a1039e
lcy12384 <15269230591@163.com>
2024-03-16 20:04:09 +0800

    增加数据库保存课程目录信息

--- 数据库连接.py
+++ 数据库连接.py
@@ -27,20 +27,41 @@ def save_inf(i,cursor):
         for line in load_line:
             l = line.strip()
             dic = json.loads(l)
-            sql = (f"insert into course_inf values ({int(dic['id'])},'{dic['name']}',"
+            #课程基础信息
+            sql_1 = (f"insert into course_inf values ({int(dic['id'])},'{dic['name']}',"
                    f"'{dic['teacher']}','{dic['school']}',{float(dic['scores'])},{int(dic['commentsCount'])},"
                    f"{int(dic['chooseCourseCount'])},{int(dic['curChooseCourseCount'])},{int(dic['curBbsAllCount'])},"
                    f"{int(dic['bbsAllCount'])},{int(dic['viewTimes'])},'{dic['url']}','{dic['courseLevel']}')")
-            cursor.execute(sql)
+            cursor.execute(sql_1)
+            #课程开设期次
+            sql_2 = f"insert into course_time values ({int(dic['id'])},'{dic['qici'][-1][0]}','{dic['endtime'][0]}')"
+            cursor.execute(sql_2)
         load_f.close()
     except Exception as e:
         print("异常:",e)
 
+def save_dir(i,cursor):
+    try:
+        load_f = open(f"D:/WorkSpace/毕业设计/xueyin_course_dir-2024.3.7/page{i}.txt", "r", encoding="utf-8")
+        load_line = load_f.readlines()
+        for line in load_line:
+            l = line.strip()
+            dic = json.loads(l)
+            i = 1
+            for dir in dic['dir']:
+                sql = f"insert into course_dir values ({int(dic['id'])},'{dir}',{i})"
+                cursor.execute(sql)
+                i += 1
+    except Exception as e:
+        print("异常:", e)
+
 def main():
     cursor = cre_mysql()
     for i in range(1,1056):
-        save_inf(i,cursor)
-        print(f"第{i}页课程基础信息输入完成！")
+        # save_inf(i,cursor)
+        # print(f"第{i}页课程基础信息输入完成！")
+        save_dir(i,cursor)
+        print(f"第{i}页课程目录信息输入完成！")
     if cursor:
         # 关闭连接
         cursor.close()
